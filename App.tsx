f745593e215c35abe97ca4aa279c2bd6d7611ae2
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-07-01 22:24:30 -0300

    create signin sreen route

--- App.tsx
+++ App.tsx
@@ -6,18 +6,21 @@ import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components/native';
 import theme from './src/theme';
 
+import { SignIn } from './src/screens';
+
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    DMSans_400Regular,
-    DMSerifDisplay_400Regular,
-  });
+	const [fontsLoaded] = useFonts({
+		DMSans_400Regular,
+		DMSerifDisplay_400Regular,
+	});
 
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  }
+	if (!fontsLoaded) {
+		return <AppLoading />;
+	}
 
-  return (
-    <ThemeProvider theme={theme}>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<SignIn />
+		</ThemeProvider>
+	);
 }
