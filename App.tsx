cf885f9150a345381dd9633941b5eabab933c4e9
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-06-29 18:11:40 -0300

    chore: configure project

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import AppLoading from 'expo-app-loading';
+import { View } from 'react-native';
+import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
+import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
+
+export default function App() {
+	const [fontsLoaded] = useFonts({
+		DMSans_400Regular,
+		DMSerifDisplay_400Regular,
+	});
+
+	if (!fontsLoaded) {
+		return <AppLoading />;
+	}
+
+	return <View></View>;
+}
