c16949deb0d5fb0e019b4c5012da521ddecec308
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-06-30 03:17:43 -0300

    chore: create sign in screen

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { KeyboardAvoidingView, Platform } from 'react-native';
+import { Button, Input } from '../../components';
+import {
+	Brand,
+	Container,
+	Content,
+	ForgotPasswordButton,
+	ForgotPasswordLabel,
+	Title,
+} from './styles';
+
+import brandImg from '../../assets/brand.png';
+
+export function SignIn() {
+	return (
+		<Container>
+			<KeyboardAvoidingView
+				behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+			>
+				<Content>
+					<Brand source={brandImg} />
+					<Title>Login</Title>
+					<Input
+						placeholder='E-mail'
+						type='secondary'
+						autoCorrect={false}
+						autoCapitalize='none'
+					/>
+					<Input placeholder='Senha' type='secondary' secureTextEntry />
+					<ForgotPasswordButton>
+						<ForgotPasswordLabel>Esqueci minha senha</ForgotPasswordLabel>
+					</ForgotPasswordButton>
+					<Button title='Entrar' type='secondary' />
+				</Content>
+			</KeyboardAvoidingView>
+		</Container>
+	);
+}
